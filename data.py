e8931479aef51fdb06893dcc309f53ab8da2d104
Dairon Manzo <9381010420az@gmail.com>
2026-05-16 14:17:50 -0600

    Configurar servidor y gitignore

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://029f5e89-1ed2-457a-8c61-c3ff8b9e57c2.serverhub.tripleten-services.com'
+URBAN_ROUTES_URL = 'https://cnt-f0bfdffc-6aab-4f04-a459-39a8b17bdf86.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
