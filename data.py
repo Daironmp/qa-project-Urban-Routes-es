bab824f17e2473a5c3fc5b8fd707f7b5c425f5eb
Dairon Manzo <9381010420az@gmail.com>
2026-05-16 21:37:59 -0600

    Actualizar proyecto y agregar README

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
-URBAN_ROUTES_URL = 'https://cnt-f0bfdffc-6aab-4f04-a459-39a8b17bdf86.containerhub.tripleten-services.com?lng=es'
-address_from = 'East 2nd Street, 601'
-address_to = '1300 1st St'
-phone_number = '+1 123 123 12 12'
-card_number, card_code = '1234 5678 9100', '111'
-message_for_driver = 'Muéstrame el camino al museo'
+URBAN_ROUTES_URL = "https://cnt-a5f6ac9c-7c07-455d-894d-48aeb8c03a97.containerhub.tripleten-services.com?lng=es"
+
+address_from = "East 2nd Street, 601"
+address_to = "1300 1st St"
+phone_number = "+11231231212"
+card_number = "1234123412341234"
+card_code = "123"
+message_for_driver = "Muéstrame el camino al museo"
